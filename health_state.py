cbeca302beae30a8f68dbd7c05392ed761f6f180
leonluong1 <46467225+leonluong1@users.noreply.github.com>
2019-03-08 15:00:57 -0800

    Add files via upload

--- health_state.py
+++ health_state.py
@@ -1,7 +1,8 @@
+import questionnaire as quest
 
 def get_name():
-    s = "state_"
-    name = input("What is your name?")
+    s = "state_questionnaire_"
+    name = input("What is your name?: ")
     nameList = name.split()
     for word in nameList:
         if (word != nameList[-1]):
@@ -11,21 +12,69 @@ def get_name():
     s += ".txt"
     return s
 
-def read_state(f):
-    pass
+def get_values(q):
+    newValues = [str(q.iterations), q.v1.get(), q.v2.get(), q.v3.get(), q.v4.get(), q.v5.get(), q.v6.get(), 
+        q.v7.get(), q.v8.get(), q.v9.get(), q.v10.get(), q.v11.get(), q.v12.get(), q.v13.get()]
+    return newValues
 
-def write_state2(f):
-    pass
+def read_state(f):
+    oldValues = []
+    avgValues = []
+    section = "ITERATION"
+    for value in f.readlines():
+        if (value.strip() == "RECENT"):
+            section = "RECENT"
+        elif (value.strip() == "AVERAGE"):
+            section = "AVERAGE"
+        elif (section == "RECENT"):
+            oldValues.append(value.strip())
+        elif (section == "AVERAGE"):
+            avgValues.append(value.strip())
+        else:
+            oldValues.append(value.strip())
+            avgValues.append(value.strip())
+    return oldValues, avgValues
+
+def write_state_questionnaire(f, oldValues, avgValues, userCheck):
+    q = quest.Questionnaire(oldValues)
+    q.start()
+    if ((q.confirm) == False):
+        return 0
+
+    f.seek(0)
+    f.truncate()
+    newValues = get_values(q)
+    f.write(newValues[0] + "\n")
+
+    f.write("RECENT\n")
+    for i in range(1, 14):
+        f.write(newValues[i] + "\n")
+
+    f.write("AVERAGE\n")
+    if (userCheck == "new"):
+        for i in range(1, 14):
+            f.write(newValues[i] + "\n")
+    else:
+        for i in range(1, 14):
+            f.write(str((int(newValues[i]) + (float(avgValues[i]) * int(avgValues[0])))/
+                int(newValues[0]))  + "\n")
+    return 0
 
 def write_state():
     s = get_name()
+    answer = input("Do you want to take the questionnaire? (yes/no): ")
+    if (answer != "yes"):
+        return 0
     try:
-        f = open(s, "r")
-        read_state(f)
-        write_state2(f)
-    except:
-        f = open(s, 'w+')
-        write_state2(f)
+        f = open(s, "r+")
+        oldValues, avgValues = read_state(f)
+        write_state_questionnaire(f, oldValues, avgValues, "existing")
+        f.close()
+    except FileNotFoundError:
+        f1 = open(s, 'w+')
+        oldValues = [0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+        write_state_questionnaire(f1, oldValues, [], "new")
+        f1.close()
 
 if __name__ == '__main__':
-    pass
+    write_state()
--- questionnaire.py
+++ questionnaire.py
@@ -4,158 +4,182 @@ import health_state
 
 class Questionnaire:
    
-    def __init__(self):
+    def __init__(self, oldValues):
         self.TITLE_FONT = ('Arial', 16)
         self.FONT = ('Arial', 12)
         self.root = Tk()
+        OPTIONS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-        title = Label(self.root, text="Mental Health Questionnaire", 
-            font = self.TITLE_FONT)
+        title = Label(self.root, text = "Mental Health Questionnaire", font = self.TITLE_FONT)
         
-        title.grid(row = 0, column = 1, columnspan = 2, padx = 10, pady = 10, sticky = N)
+        title.grid(row = 0, column = 1, columnspan = 1, padx = 10, pady = 8, sticky = N)
 
-        q1 = Label(self.root, text = "How do you feel?", font = self.FONT)
+        rating = Label(self.root, text = "Rate from 0 (extremely disagree) to 9 (extremely agree)", 
+            font = self.FONT)
 
-        q1.grid(row = 1, column = 0, columnspan = 1, padx = 10, pady = 6, sticky = W)
+        rating.grid(row = 1, column = 1, columnspan = 1, padx = 10, pady = 7, sticky = N)
 
-        self.q1v = StringVar(self.root)
-        self.q1v.set("Average")
+        q1 = Label(self.root, text = "Do you blame yourself for everything bad that happens?", 
+            font = self.FONT)
 
-        q1Options = OptionMenu(self.root, self.q1v, "Terrible", "Bad", "Average", "Good", "Great")
+        q1.grid(row = 2, column = 0, columnspan = 1, padx = 10, pady = 5, sticky = W)
+
+        self.v1 = StringVar(self.root)
+        self.v1.set(oldValues[1])
+
+        q1Options = OptionMenu(self.root, self.v1, *OPTIONS)
     
-        q1Options.grid(row = 1, column = 2, columnspan = 2, sticky = E)
+        q1Options.grid(row = 2, column = 2, columnspan = 2, sticky = E)
 
 
-        q2 = Label(self.root, text = "How do you feel?", font = self.FONT)
+        q2 = Label(self.root, text = "Do you feel you would kill yourself if you had the chance?", 
+            font = self.FONT)
 
-        q2.grid(row = 2, column = 0, columnspan = 1, padx = 10, pady = 6, sticky = W)
+        q2.grid(row = 3, column = 0, columnspan = 1, padx = 10, pady = 5, sticky = W)
 
-        self.q2v = StringVar(self.root)
-        self.q2v.set("Average")
+        self.v2 = StringVar(self.root)
+        self.v2.set(oldValues[2])
 
-        q2Options = OptionMenu(self.root, self.q2v, "Terrible", "Bad", "Average", "Good", "Great")
+        q2Options = OptionMenu(self.root, self.v2, *OPTIONS)
     
-        q2Options.grid(row = 2, column = 2, columnspan = 2, sticky = E)
+        q2Options.grid(row = 3, column = 2, columnspan = 2, sticky = E)
 
 
-        q3 = Label(self.root, text = "How do you feel?", font = self.FONT)
+        q3 = Label(self.root, text = "Do you feel you are being punished?", font = self.FONT)
 
-        q3.grid(row = 3, column = 0, columnspan = 1, padx = 10, pady = 6, sticky = W)
+        q3.grid(row = 4, column = 0, columnspan = 1, padx = 10, pady = 5, sticky = W)
 
-        self.q3v = StringVar(self.root)
-        self.q3v.set("Average")
+        self.v3 = StringVar(self.root)
+        self.v3.set(oldValues[3])
 
-        q3Options = OptionMenu(self.root, self.q3v, "Terrible", "Bad", "Average", "Good", "Great")
+        q3Options = OptionMenu(self.root, self.v3, *OPTIONS)
     
-        q3Options.grid(row = 3, column = 2, columnspan = 2, sticky = E)
+        q3Options.grid(row = 4, column = 2, columnspan = 2, sticky = E)
 
 
-        q4 = Label(self.root, text = "How do you feel?", font = self.FONT)
+        q4 = Label(self.root, text = "Do you feel the future is hopeless and that things cannot improve?", 
+            font = self.FONT)
 
-        q4.grid(row = 4, column = 0, columnspan = 1, padx = 10, pady = 6, sticky = W)
+        q4.grid(row = 5, column = 0, columnspan = 1, padx = 10, pady = 5, sticky = W)
 
-        self.q4v = StringVar(self.root)
-        self.q4v.set("Average")
+        self.v4 = StringVar(self.root)
+        self.v4.set(oldValues[4])
 
-        q4Options = OptionMenu(self.root, self.q4v, "Terrible", "Bad", "Average", "Good", "Great")
+        q4Options = OptionMenu(self.root, self.v4, *OPTIONS)
     
-        q4Options.grid(row = 4, column = 2, columnspan = 2, sticky = E)
+        q4Options.grid(row = 5, column = 2, columnspan = 2, sticky = E)
 
 
-        q5 = Label(self.root, text = "How do you feel?", font = self.FONT)
+        q5 = Label(self.root, text = 
+            "Are you fulfilling your responsibilities such as your career or relationships?", font = self.FONT)
 
-        q5.grid(row = 5, column = 0, columnspan = 1, padx = 10, pady = 6, sticky = W)
+        q5.grid(row = 6, column = 0, columnspan = 1, padx = 10, pady = 5, sticky = W)
 
-        self.q5v = StringVar(self.root)
-        self.q5v.set("Average")
+        self.v5 = StringVar(self.root)
+        self.v5.set(oldValues[5])
 
-        q5Options = OptionMenu(self.root, self.q5v, "Terrible", "Bad", "Average", "Good", "Great")
+        q5Options = OptionMenu(self.root, self.v5, *OPTIONS)
     
-        q5Options.grid(row = 5, column = 2, columnspan = 2, sticky = E)
+        q5Options.grid(row = 6, column = 2, columnspan = 2, sticky = E)
 
 
-        q6 = Label(self.root, text = "How do you feel?", font = self.FONT)
+        q6 = Label(self.root, text = " Do you feel that you can’t make decisions at all anymore?", 
+            font = self.FONT)
 
-        q6.grid(row = 6, column = 0, columnspan = 1, padx = 10, pady = 6, sticky = W)
+        q6.grid(row = 7, column = 0, columnspan = 1, padx = 10, pady = 5, sticky = W)
 
-        self.q6v = StringVar(self.root)
-        self.q6v.set("Average")
+        self.v6 = StringVar(self.root)
+        self.v6.set(oldValues[6])
 
-        q6Options = OptionMenu(self.root, self.q6v, "Terrible", "Bad", "Average", "Good", "Great")
+        q6Options = OptionMenu(self.root, self.v6, *OPTIONS)
     
-        q6Options.grid(row = 6, column = 2, columnspan = 2, sticky = E)
+        q6Options.grid(row = 7, column = 2, columnspan = 2, sticky = E)
 
 
-        q7 = Label(self.root, text = "How do you feel?", font = self.FONT)
+        q7 = Label(self.root, text = "Are you too tired to do anything?", font = self.FONT)
 
-        q7.grid(row = 7, column = 0, columnspan = 1, padx = 10, pady = 6, sticky = W)
+        q7.grid(row = 8, column = 0, columnspan = 1, padx = 10, pady = 5, sticky = W)
 
-        self.q7v = StringVar(self.root)
-        self.q7v.set("Average")
+        self.v7 = StringVar(self.root)
+        self.v7.set(oldValues[7])
 
-        q7Options = OptionMenu(self.root, self.q7v, "Terrible", "Bad", "Average", "Good", "Great")
+        q7Options = OptionMenu(self.root, self.v7, *OPTIONS)
     
-        q7Options.grid(row = 7, column = 2, columnspan = 2, sticky = E)
+        q7Options.grid(row = 8, column = 2, columnspan = 2, sticky = E)
 
 
-        q8 = Label(self.root, text = "How do you feel?", font = self.FONT)
+        q8 = Label(self.root, text = "Are you so sad and unhappy that you can’t stand it?", font = self.FONT)
 
-        q8.grid(row = 8, column = 0, columnspan = 1, padx = 10, pady = 6, sticky = W)
+        q8.grid(row = 9, column = 0, columnspan = 1, padx = 10, pady = 5, sticky = W)
 
-        self.q8v = StringVar(self.root)
-        self.q8v.set("Average")
+        self.v8 = StringVar(self.root)
+        self.v8.set(oldValues[8])
 
-        q8Options = OptionMenu(self.root, self.q8v, "Terrible", "Bad", "Average", "Good", "Great")
+        q8Options = OptionMenu(self.root, self.v8, *OPTIONS)
     
-        q8Options.grid(row = 8, column = 2, columnspan = 2, sticky = E)
+        q8Options.grid(row = 9, column = 2, columnspan = 2, sticky = E)
 
 
-        q9 = Label(self.root, text = "How do you feel?", font = self.FONT)
+        q9 = Label(self.root, text = "Do you feel irritated all the time?", font = self.FONT)
 
-        q9.grid(row = 9, column = 0, columnspan = 1, padx = 10, pady = 6, sticky = W)
+        q9.grid(row = 10, column = 0, columnspan = 1, padx = 10, pady = 5, sticky = W)
 
-        self.q9v = StringVar(self.root)
-        self.q9v.set("Average")
+        self.v9 = StringVar(self.root)
+        self.v9.set(oldValues[9])
 
-        q9Options = OptionMenu(self.root, self.q9v, "Terrible", "Bad", "Average", "Good", "Great")
+        q9Options = OptionMenu(self.root, self.v9, (*OPTIONS))
     
-        q9Options.grid(row = 9, column = 2, columnspan = 2, sticky = E)
+        q9Options.grid(row = 10, column = 2, columnspan = 2, sticky = E)
 
 
-        q10 = Label(self.root, text = "How do you feel?", font = self.FONT)
+        q10 = Label(self.root, text = "Are you dissatisfied or bored with everything?", font = self.FONT)
 
-        q10.grid(row = 10, column = 0, columnspan = 1, padx = 10, pady = 6, sticky = W)
+        q10.grid(row = 11, column = 0, columnspan = 1, padx = 10, pady = 5, sticky = W)
 
-        self.q10v = StringVar(self.root)
-        self.q10v.set("Average")
+        self.v10 = StringVar(self.root)
+        self.v10.set(oldValues[10])
 
-        q10Options = OptionMenu(self.root, self.q10v, "Terrible", "Bad", "Average", "Good", "Great")
+        q10Options = OptionMenu(self.root, self.v10, *OPTIONS)
     
-        q10Options.grid(row = 10, column = 2, columnspan = 2, sticky = E)
+        q10Options.grid(row = 11, column = 2, columnspan = 2, sticky = E)
 
 
-        q11 = Label(self.root, text = "How do you feel?", font = self.FONT)
+        q11 = Label(self.root, text = "Have you lost all of your interest in other people?", font = self.FONT)
 
-        q11.grid(row = 11, column = 0, columnspan = 1, padx = 10, pady = 6, sticky = W)
+        q11.grid(row = 12, column = 0, columnspan = 1, padx = 10, pady = 5, sticky = W)
 
-        self.q11v = StringVar(self.root)
-        self.q11v.set("Average")
+        self.v11 = StringVar(self.root)
+        self.v11.set(oldValues[11])
 
-        q11Options = OptionMenu(self.root, self.q11v, "Terrible", "Bad", "Average", "Good", "Great")
+        q11Options = OptionMenu(self.root, self.v11, *OPTIONS)
     
-        q11Options.grid(row = 11, column = 2, columnspan = 2, sticky = E)
+        q11Options.grid(row = 12, column = 2, columnspan = 2, sticky = E)
 
+        
+        q12 = Label(self.root, text = "Have you been out of the house recently?", 
+            font = self.FONT)
 
-        q12 = Label(self.root, text = "How do you feel?", font = self.FONT)
+        q12.grid(row = 13, column = 0, columnspan = 1, padx = 10, pady = 5, sticky = W)
 
-        q12.grid(row = 12, column = 0, columnspan = 1, padx = 10, pady = 6, sticky = W)
+        self.v12 = StringVar(self.root)
+        self.v12.set(oldValues[12])
 
-        self.q12v = StringVar(self.root)
-        self.q12v.set("Average")
-
-        q12Options = OptionMenu(self.root, self.q12v, "Terrible", "Bad", "Average", "Good", "Great")
+        q12Options = OptionMenu(self.root, self.v12, *OPTIONS)
     
-        q12Options.grid(row = 12, column = 2, columnspan = 2, sticky = E)
+        q12Options.grid(row = 13, column = 2, columnspan = 2, sticky = E)
+
+
+        q13 = Label(self.root, text = "Do you have very little or no appetite at all anymore?", 
+            font = self.FONT)
+
+        q13.grid(row = 14, column = 0, columnspan = 1, padx = 10, pady = 5, sticky = W)
+
+        self.v13 = StringVar(self.root)
+        self.v13.set(oldValues[13])
+
+        q13Options = OptionMenu(self.root, self.v13, *OPTIONS)
+    
+        q13Options.grid(row = 14, column = 2, columnspan = 2, sticky = E)
 
 
         buttonFrame = Frame(master = self.root)
@@ -172,24 +196,12 @@ class Questionnaire:
 
         cancelButton.grid(row = 0, column = 1,  pady = 10)
 
-        self.okay = False
+        self.confirm = False
+        self.iterations = int(oldValues[0]) + 1
 
 
     def _on_confirm(self):
-        self.okay = True
-        print(self.q1v.get())
-        print(self.q2v.get())
-        print(self.q3v.get())
-        print(self.q4v.get())
-        print(self.q5v.get())
-        print(self.q6v.get())
-        print(self.q7v.get())
-        print(self.q8v.get())
-        print(self.q9v.get())
-        print(self.q10v.get())
-        print(self.q11v.get())
-        print(self.q12v.get())
-
+        self.confirm = True
         self.root.destroy()
 
     def _on_cancel(self):
@@ -202,4 +214,5 @@ class Questionnaire:
 
 
 if __name__ == '__main__':
-    Questionnaire().start()
+    oldValues = [0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    Questionnaire(oldValues).start()
